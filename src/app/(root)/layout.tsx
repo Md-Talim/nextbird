318e39e1b71bea7af6c7dfa07a71ee691fc13a8c
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-08 10:03:33 +0530

    Declare the RightSidebar component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -5,6 +5,7 @@ import '../globals.css';
 import Topbar from '@/components/shared/Topbar';
 import Bottombar from '@/components/shared/Bottombar';
 import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,7 +27,7 @@ export default function RootLayout({
           <section className="main-container">
             <LeftSidebar />
             <div className="w-full max-w-4xl">{children}</div>
-            {/* RightSideBar */}
+            <RightSidebar />
           </section>
           <Bottombar />
         </body>
