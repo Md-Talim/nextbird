ff05bd29a865726a3b3bb780553bbdbbc84436c2
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-08 11:35:59 +0530

    Redirect to sign-in page after logging out

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -33,7 +33,7 @@ const LeftSidebar = () => {
 
       <div className="mt-10 px-6">
         <SignedIn>
-          <SignOutButton>
+          <SignOutButton signOutCallback={() => router.push('/sign-in')}>
             <div className="leftsidebar_link">
               <Image
                 src="/assets/logout.svg"
