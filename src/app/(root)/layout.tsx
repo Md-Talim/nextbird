faf09606fb69e3f463f891fed256bd9e35156868
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-08 10:01:36 +0530

    Declare the Bottombar component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../globals.css';
 import Topbar from '@/components/shared/Topbar';
+import Bottombar from '@/components/shared/Bottombar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -26,7 +27,7 @@ export default function RootLayout({
             <div className="w-full max-w-4xl">{children}</div>
             {/* RightSideBar */}
           </section>
-          {/* BottomBar */}
+          <Bottombar />
         </body>
       </html>
     </ClerkProvider>
