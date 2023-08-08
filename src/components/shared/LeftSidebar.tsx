74b3a8e08b477c7a95fafa2d2d38818021925a1a
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-08 11:20:21 +0530

    Build the left side bar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,5 +1,53 @@
+'use client';
+
+import { sidebarLinks } from '@/constants';
+import { useRouter, usePathname } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignOutButton, SignedIn } from '@clerk/nextjs';
+
 const LeftSidebar = () => {
-  return <div>LeftSidebar</div>;
+  const router = useRouter();
+  const pathname = usePathname();
+
+  return (
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex flex-col w-full flex-1 gap-6 px-6">
+        {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`leftsidebar_link ${isActive && 'bg-primary-500'}`}
+            >
+              <Image src={link.imgURL} width={24} height={24} alt="link-icon" />
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
+            </Link>
+          );
+        })}
+      </div>
+
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton>
+            <div className="leftsidebar_link">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
+      </div>
+    </section>
+  );
 };
 
 export default LeftSidebar;
