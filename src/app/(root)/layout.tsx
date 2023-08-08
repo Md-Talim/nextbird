3001107f6fa22091b250f2e961479b1d41b53be6
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-08 10:00:31 +0530

    Declare the Topbar component

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '../globals.css';
+import Topbar from '@/components/shared/Topbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {/* Topbar */}
+          <Topbar />
           <section className="main-container">
             {/* LeftSideBar */}
             <div className="w-full max-w-4xl">{children}</div>
