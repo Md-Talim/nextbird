aae6f795f1a730af7344eb5a7a5230edec53888c
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-12 11:51:56 +0530

    Fetching the posts on homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,36 @@
-export default function Home() {
+import ThreadCard from '@/components/cards/ThreadCard';
+import { fetchPosts } from '@/lib/actions/thread.actions';
+import { currentUser } from '@clerk/nextjs';
+
+export default async function Home() {
+  const results = await fetchPosts(1, 30);
+  const user = await currentUser();
+
   return (
     <div>
       <h1 className="head-text">Home</h1>
+
+      <section className="flex flex-col mt-10 gap-10">
+        {results.posts.length === 0 ? (
+          <p className="no-result">No posts found.</p>
+        ) : (
+          <>
+            {results.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id || ''}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                commuity={post.commuity}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </div>
   );
 }
