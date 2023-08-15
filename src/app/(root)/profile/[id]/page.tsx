e71363763af9274af3076d9953de21daa5048c7c
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-15 13:20:24 +0530

    Create the tab list in the profile page

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -1,6 +1,9 @@
 import ProfileHeader from '@/components/shared/ProfileHeader';
+import { profileTabs } from '@/constants';
 import { fetchUser } from '@/lib/actions/user.actions';
 import { currentUser } from '@clerk/nextjs';
+import { Tabs, TabsList, TabsTrigger } from '@radix-ui/react-tabs';
+import Image from 'next/image';
 import { redirect } from 'next/navigation';
 
 async function Page({ params }: { params: { id: string } }) {
@@ -20,6 +23,31 @@ async function Page({ params }: { params: { id: string } }) {
         bio={userInfo.bio}
         imageUrl={userInfo.image}
       />
+
+      <div className="mt-10">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.value} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === 'Threads' && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo.threads.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </Tabs>
+      </div>
     </section>
   );
 }
